135ce70e0d907438d6fd81a375e13edd8e8b0f24
Alexandre Adamski <aadamski@quarkslab.com>
2018-11-28 13:35:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# AMIE
+
+**A** **M**inimalist **I**nstruction **E**xtender
+
+AMIE is a Python rework of [FRIEND](https://github.com/alexhude/FRIEND/) that focuses solely on the ARM architecture (only AArch32 and AArch64 are supported). It is both lightweight and dependency-free, and provides the most relevant and up-to-date information about the ARM system registers and instructions.
+
+## Features
+
+### Improved processor modules
+
+For `MCR/MRC` and `MCRR/MRCC` instructions on AArch32, and for `MSR/MRS` and `SYS` instructions on AArch64, the system register encoding is detected and replaced by its user-friendly name in the *IDA View* subview.
+
+<p align="center"><img src="https://i.imgur.com/OOhEgpf.gif"></p>
+
+For `MCR/MRC` and `MSR/MRS` instructions, it also applies to the *Pseudocode* subview.
+
+<p align="center"><img src="https://i.imgur.com/ekYV1hZ.png"></p>
+
+### Hints for instructions and registers
+
+Hovering over a system register in the *IDA View* subview or in the *Pseudocode* subview will display a summary (usually kept under 30 lines) of the relevant documentation page, including the bitfield when available.
+
+<p align="center"><img src="https://i.imgur.com/GK0G8EG.png"></p>
+
+Hovering over an instruction mnemonic in the *IDA View* subview or in the *Pseudocode* subview will also display a summary of the relevant documentation page, and the relevant assembly template when available.
+
+<p align="center"><img src="https://i.imgur.com/S88dDBy.png"></p>
+
+### Auto-generated resource files
+
+The biggest difference with FRIEND is that the resource files (`aarch32.json` and `aarch64.json`) are auto-generated from the [Exploration Tools](https://developer.arm.com/products/architecture/cpu-architecture/a-profile/exploration-tools). The system registers and instructions (documentation and encodings) are extracted by a home-made script that parses the ARM-provided XML files.
+
+## Installation
+
+Copy the plugin file `amie.py`, and its resource files `aarch32.json` and `aarch64.json` to your plugins directory or your user plugins directory (if you want to share it between multiple IDA Pro versions). These are the default paths:
+
+OS      | Plugins Directory                          | User Plugins Directory
+--------|--------------------------------------------|-------------------------------------
+Windows | `%PROGRAMFILES%\IDA 7.2\plugins`           | `%APPDATA%\Hex-Rays\IDA Pro\plugins`
+Linux   | `~/ida-7.2/plugins`                        | `~/.idapro/plugins`
+macOS   | `/Applications/IDA Pro 7.2/idabin/plugins` | `~/.idapro/plugins`
+
+## Dependencies
+
+There are no dependencies! :-)
+
+## Improvements
+
+Support for implementation-defined system registers is not available yet.
+
+There is no Hex-Rays support for `MCRR/MRCC` as this is an IDA Pro limitation.
+
+## Credits
+
+* [alexhude](https://github.com/alexhude) for creating the [FRIEND](https://github.com/alexhude/FRIEND/) plugin;
+* [gdelugre](https://github.com/gdelugre/) for creating the [ida-arm-system-highlight](https://github.com/gdelugre/ida-arm-system-highlight/) script;
+* The good folks at ARM for releasing the [Exploration Tools](https://developer.arm.com/products/architecture/cpu-architecture/a-profile/exploration-tools);
+* [patateqbool](https://github.com/patateqbool) and [0xpanda](https://github.com/0xpanda) for testing the plugin and reporting bugs;
+* Quarkslab for allowing this release.
---
+++ amie.py
@@ -0,0 +1,437 @@
+import inspect
+import json
+import os
+import re
+import struct
+
+import ida_allins
+import ida_bytes
+import ida_hexrays
+import ida_idaapi
+import ida_idp
+import ida_kernwin
+import ida_lines
+import ida_segregs
+import ida_ua
+
+
+class Arch(object):
+    @staticmethod
+    def output_insn(outctx, mne, dst_reg, src_reg=None):
+        outctx.out_custom_mnem(mne.upper().encode("utf-8"), 16)
+        outctx.out_register(dst_reg.encode("utf-8"))
+        if src_reg:
+            outctx.out_printf(", ")
+            outctx.out_register(src_reg.encode("utf-8"))
+        outctx.flush_outbuf()
+        return True
+
+    def __init__(self, file_name):
+        cur_file = inspect.getsourcefile(lambda: 0)
+        cur_path = os.path.dirname(os.path.abspath(cur_file))
+        with open(os.path.join(cur_path, file_name), "r") as fd:
+            self.data = json.loads(fd.read())
+
+        self.regs = {}
+        self.regs.update(self.data["registers"]["base"])
+        self.regs.update(self.data["registers"]["instructions"])
+        self.regs.update(self.data["registers"]["registers"])
+        self.regs_enc = self.data["registers"]["encodings"]
+
+        self.insns = {}
+        self.insns.update(self.data["instructions"]["base"])
+        self.insns.update(self.data["instructions"]["simd_fp"])
+        self.insns_enc = self.data["instructions"]["encodings"]
+
+    def matches(self, op, reg_op):
+        op = "{:0{size}b}".format(op, size=len(reg_op))[: len(reg_op)]
+        for i in range(len(op)):
+            if op[i] != reg_op[i] and reg_op[i] != "x":
+                return False
+        return True
+
+    def find_reg_enc(self, enc_type, ops):
+        for cp_reg_name, reg_ops in self.regs_enc[enc_type].items():
+            for i in range(len(ops)):
+                if not self.matches(ops[i], reg_ops[i]):
+                    break
+            else:
+                return cp_reg_name
+        print("[AMIE] %s encoding not found for: %s" % (enc_type, ops))
+
+    def find_insn_enc(self, enc_type, insn_bs, insn_mne):
+        for insn_name, insn_encs in self.insns_enc[enc_type].items():
+            for tmpl_name, insn_enc in insn_encs.items():
+                match = self.matches(insn_bs, insn_enc[0])
+                for enc in insn_enc[1:]:
+                    match = match and not self.matches(insn_bs, enc)
+                if not match:
+                    continue
+
+                for tmpls in self.insns[insn_name]["templates"].values():
+                    for tmpl in tmpls:
+                        mne = tmpl.split(" ")[0]
+                        mne = re.sub("{[^}]}", "", mne)
+                        mne = re.sub("<[^>]>", "", mne)
+                        if mne.startswith(insn_mne):
+                            return insn_name, tmpl_name
+        print("[AMIE] %s encoding not found for: %s" % (enc_type, insn_mne))
+
+    def output(self, outctx):
+        raise NotImplemented()
+
+    def hint(self, ea, tag, val):
+        val = val.strip()
+        if not val:
+            return None
+
+        if tag == ida_lines.SCOLOR_REG:
+            if val in self.regs:
+                register = self.regs[val]
+                return register["long_name"], register["purpose"]
+
+        elif tag == ida_lines.SCOLOR_INSN:
+            insn = ida_ua.insn_t()
+            ida_ua.decode_insn(insn, ea)
+            insn_bs = ida_bytes.get_bytes(insn.ea, insn.size)
+            fmt = {2: "<H", 4: "<I", 8: "<Q"}
+            insn_bs = struct.unpack(fmt.get(len(insn_bs)), insn_bs)[0]
+
+            enc_type = "A64"
+            if isinstance(self, AArch32):
+                enc_type = "A32"
+                if ida_segregs.get_sreg(ea, 20) > 0:
+                    enc_type = "T32"
+
+            insn_enc = self.find_insn_enc(
+                enc_type, insn_bs, insn.get_canon_mnem()
+            )
+            if not insn_enc:
+                return
+            insn_name, tmpl_name = insn_enc
+            insn = self.insns[insn_name]
+
+            desc = insn["authored"]
+            if tmpl_name in insn["templates"]:
+                desc += "\n\n" + "\n".join(insn["templates"][tmpl_name])
+            return insn["heading"], desc
+
+        elif tag == ida_lines.SCOLOR_KEYWORD:
+            if val in self.data["keywords"]:
+                return val, self.data["keywords"][val]
+
+
+class AArch32(Arch):
+    def __init__(self):
+        super(AArch32, self).__init__("aarch32.json")
+
+    def decode_mcr_mrc(self, insn):
+        cp = insn.ops[0].specflag1
+        op1 = insn.ops[0].value
+        rt = insn.ops[1].reg
+        crn = insn.ops[1].specflag1
+        crm = insn.ops[1].specflag2
+        op2 = insn.ops[2].value
+
+        ops = [cp, op1, crn, crm, op2]
+        gp_reg = ida_idp.ph.regnames[rt]
+        cp_reg = self.find_reg_enc("MCR|MRC", ops)
+        return cp_reg, gp_reg
+
+    def output(self, outctx):
+        insn = outctx.insn
+        mne = ida_ua.print_insn_mnem(insn.ea)
+
+        if insn.itype in [ida_allins.ARM_mcr, ida_allins.ARM_mrc]:
+            cp_reg, gp_reg = self.decode_mcr_mrc(insn)
+            if cp_reg:
+                if insn.itype == ida_allins.ARM_mcr:
+                    return Arch.output_insn(outctx, mne, cp_reg, gp_reg)
+                else:
+                    return Arch.output_insn(outctx, mne, gp_reg, cp_reg)
+
+        if insn.itype in [ida_allins.ARM_mcrr, ida_allins.ARM_mrrc]:
+            cp = insn.ops[0].specflag1
+            op3 = insn.ops[0].value
+            rt = insn.ops[1].reg
+            rt2 = insn.ops[1].specflag1
+            crm = insn.ops[1].specflag2
+
+            ops = [cp, op3, crm]
+            gp_reg = "%s%s%s:%s%s%s" % (
+                ida_idp.ph.regnames[rt2],
+                ida_lines.SCOLOR_OFF,
+                ida_lines.SCOLOR_REG,
+                ida_lines.SCOLOR_ON,
+                ida_lines.SCOLOR_REG,
+                ida_idp.ph.regnames[rt],
+            )
+            cp_reg = self.find_reg_enc("MCRR|MRRC", ops)
+            if cp_reg:
+                if insn.itype == ida_allins.ARM_mcrr:
+                    return Arch.output_insn(outctx, mne, cp_reg, gp_reg)
+                else:
+                    return Arch.output_insn(outctx, mne, gp_reg, cp_reg)
+
+        return False
+
+
+class AArch64(Arch):
+    def __init__(self):
+        super(AArch64, self).__init__("aarch64.json")
+
+    def decode_msr_mrs(self, insn):
+        x = ida_bytes.get_wide_dword(insn.ea)
+        op0 = 2 + ((x >> 19) & 1)
+        if insn.itype == ida_allins.ARM_msr:
+            i, xt = 0, insn.ops[4].reg
+        else:
+            xt, i = insn.ops[0].reg, 1
+        op1 = insn.ops[i].value
+        crn = insn.ops[i + 1].reg
+        crm = insn.ops[i + 2].reg
+        op2 = insn.ops[i + 3].value
+
+        ops = [op0, op1, crn, crm, op2]
+        gp_reg = ida_idp.ph.regnames[xt]
+        cp_reg = self.find_reg_enc("MRS|MSR", ops)
+        return cp_reg, gp_reg
+
+    def output(self, outctx):
+        insn = outctx.insn
+        mne = ida_ua.print_insn_mnem(insn.ea)
+
+        if insn.itype in [ida_allins.ARM_msr, ida_allins.ARM_mrs]:
+            if insn.ops[2].type > 0:
+                cp_reg, gp_reg = self.decode_msr_mrs(insn)
+                if cp_reg:
+                    if insn.itype == ida_allins.ARM_msr:
+                        return Arch.output_insn(outctx, mne, cp_reg, gp_reg)
+                    else:
+                        return Arch.output_insn(outctx, mne, gp_reg, cp_reg)
+
+            else:
+                fields = {5: "SPSel", 6: "DAIFSet", 7: "DAIFClr"}
+                pstatefield = fields.get(insn.ops[0].value, None)
+                if pstatefield:
+                    outctx.out_custom_mnem(mne.upper().encode("utf-8"), 16)
+                    outctx.out_register(pstatefield.encode("utf-8"))
+                    outctx.out_printf(", ")
+                    imm = ida_ua.print_operand(insn.ea, 1)
+                    outctx.out_printf(imm)
+                    outctx.flush_outbuf()
+                    return True
+
+        if insn.itype == ida_allins.ARM_sys:
+            op1 = insn.ops[0].value
+            crn = insn.ops[1].reg
+            crm = insn.ops[2].reg
+            op2 = insn.ops[3].value
+            xt = insn.ops[4].reg
+
+            ops = [1, op1, crn, crm, op2]
+            gp_reg = ida_idp.ph.regnames[xt]
+            cp_reg = self.find_reg_enc("SYS", ops)
+            if cp_reg:
+                mne, op = cp_reg.split()
+                if xt == 160:
+                    return Arch.output_insn(outctx, mne, op)
+                else:
+                    return Arch.output_insn(outctx, mne, op, gp_reg)
+
+        return False
+
+
+class AMIE(ida_idaapi.plugin_t, ida_idp.IDP_Hooks, ida_kernwin.UI_Hooks):
+    flags = ida_idaapi.PLUGIN_PROC | ida_idaapi.PLUGIN_DRAW
+    comment = "A Minimalist Instruction Extender"
+    help = ""
+    wanted_name = "AMIE"
+    wanted_hotkey = ""
+    version = "0.8.5"
+
+    @staticmethod
+    def extract_item(viewer, stripped=False):
+        cnt = ida_kernwin.get_custom_viewer_place(viewer, True)[1]
+        line = ida_kernwin.get_custom_viewer_curline(viewer, True)
+
+        tags = [ida_lines.SCOLOR_ON, ida_lines.SCOLOR_OFF]
+        expr = re.compile("[A-Z0-9_]")
+
+        def find_pos(line, pos, inc):
+            while 0 <= pos < len(line):
+                if not stripped and line[pos] in tags:
+                    break
+                if stripped and not expr.match(line[pos]):
+                    break
+                pos += inc
+            return pos
+
+        pos = ida_lines.tag_advance(line, cnt)
+        if pos < 0 or pos >= len(line):
+            return
+        while line[pos] in [ida_lines.SCOLOR_ON, ida_lines.SCOLOR_OFF]:
+            pos += 2
+            if pos < 0 or pos >= len(line):
+                return
+
+        prev_pos, next_pos = find_pos(line, pos, -1), find_pos(line, pos, +1)
+        if stripped:
+            return None, line[prev_pos + 1:next_pos].strip()
+        return line[prev_pos + 1], line[prev_pos + 2:next_pos].strip()
+
+    @staticmethod
+    def format_hint(title, content):
+        def tag_append(s, t):
+            return ida_lines.SCOLOR_ON + t + s + ida_lines.SCOLOR_OFF + t
+
+        text = " " + tag_append(title, ida_lines.SCOLOR_LOCNAME)
+        if content:
+            text += "\n"
+            for line in content.split("\n"):
+                text += "\n " + tag_append(line, ida_lines.SCOLOR_AUTOCMT)
+        return text.encode("utf-8"), len(text.split("\n"))
+
+    def __init__(self):
+        ida_idp.IDP_Hooks.__init__(self)
+        ida_kernwin.UI_Hooks.__init__(self)
+        self.hexrays_support = False
+
+        info = ida_idaapi.get_inf_structure()
+        if info.procName == "ARM":
+            if info.is_64bit():
+                print("[AMIE] Using AArch64 architecture")
+                self.arch = AArch64()
+            else:
+                print("[AMIE] Using AArch32 architecture")
+                self.arch = AArch32()
+        else:
+            self.arch = None
+
+    def init(self):
+        if not self.arch:
+            return ida_idaapi.PLUGIN_SKIP
+
+        ida_kernwin.UI_Hooks.hook(self)
+        ida_idp.IDP_Hooks.hook(self)
+        print("[AMIE] Plugin v%s initialized" % AMIE.version)
+        return ida_idaapi.PLUGIN_KEEP
+
+    def term(self):
+        ida_idp.IDP_Hooks.unhook(self)
+        ida_kernwin.UI_Hooks.unhook(self)
+        if self.hexrays_support:
+            ida_hexrays.remove_hexrays_callback(self.hxe_callback)
+            ida_hexrays.term_hexrays_plugin()
+        print("[AMIE] Plugin v%s terminated" % AMIE.version)
+
+    def run(self, _):
+        return False
+
+    def plugin_loaded(self, plugin_info):
+        if plugin_info.name == "Hex-Rays Decompiler":
+            if ida_hexrays.init_hexrays_plugin():
+                self.hexrays_support = True
+                ida_hexrays.install_hexrays_callback(self.hxe_callback)
+                print("[AMIE] Hex-Rays decompiler is supported")
+
+    def ev_out_insn(self, outctx):
+        return self.arch.output(outctx)
+
+    def get_custom_viewer_hint(self, viewer, place):
+        widget_type = ida_kernwin.get_widget_type(viewer)
+        if widget_type != ida_kernwin.BWN_DISASM:
+            return
+
+        flags = ida_bytes.get_flags(place.toea()) if place else 0
+        if not ida_bytes.is_code(flags):
+            return
+
+        item = AMIE.extract_item(viewer)
+        if not item:
+            return
+        tag, val = item
+
+        hint = self.arch.hint(place.toea(), tag, val)
+        if not hint:
+            return
+
+        return AMIE.format_hint(*hint)
+
+    def hxe_callback(self, event, *args):
+        if event == ida_hexrays.hxe_maturity:
+            cfunc, new_maturity = args
+            if new_maturity != ida_hexrays.CMAT_FINAL:
+                return 0
+            plugin = self
+
+            class Visitor(ida_hexrays.ctree_visitor_t):
+                def visit_expr(self, e):
+                    if not e.x or e.x.op != ida_hexrays.cot_helper:
+                        return 0
+
+                    insn = ida_ua.insn_t()
+                    ida_ua.decode_insn(insn, e.ea)
+
+                    def make_reg(cp_reg):
+                        reg = ida_hexrays.carg_t()
+                        reg.op = ida_hexrays.cot_helper
+                        reg.helper = cp_reg.encode("utf-8")
+                        reg.exflags = ida_hexrays.EXFL_ALONE
+                        return reg
+
+                    if e.x.helper in ["__mcr", "__mrc"]:
+                        cp_reg = plugin.arch.decode_mcr_mrc(insn)[0]
+                        if cp_reg:
+                            if e.x.helper == "__mcr":
+                                e.x.helper = "_WriteStatusReg"
+                                val = ida_hexrays.carg_t()
+                                e.a[2].swap(val)
+                                e.a.clear()
+                                e.a.push_back(make_reg(cp_reg))
+                                e.a.push_back(val)
+
+                            else:
+                                e.x.helper = "_ReadStatusReg"
+                                e.a.clear()
+                                e.a.push_back(make_reg(cp_reg))
+
+                    elif e.x.helper == "ARM64_SYSREG":
+                        cp_reg = plugin.arch.decode_msr_mrs(insn)[0]
+                        if cp_reg:
+                            e.replace_by(make_reg(cp_reg))
+
+                    return 0
+
+            visitor = Visitor(ida_hexrays.CV_PARENTS)
+            visitor.apply_to_exprs(cfunc.body, None)
+
+        elif event == ida_hexrays.hxe_create_hint:
+            hint = None
+
+            e = args[0].item.e
+            if e and e.op == ida_hexrays.cot_helper:
+                hint = self.arch.hint(e.ea, ida_lines.SCOLOR_REG, e.helper)
+
+            elif e and e.op == ida_hexrays.cit_asm:
+                item = AMIE.extract_item(args[0].ct, True)
+                if item and item[1]:
+                    if item[1] in self.arch.regs:
+                        tag = ida_lines.SCOLOR_REG
+                    else:
+                        insn = ida_ua.insn_t()
+                        ida_ua.decode_insn(insn, e.ea)
+                        if item[1].startswith(insn.get_canon_mnem()):
+                            tag = ida_lines.SCOLOR_INSN
+                        else:
+                            tag = ida_lines.SCOLOR_KEYWORD
+                    hint = self.arch.hint(e.ea, tag, item[1])
+
+            if hint:
+                return (1,) + AMIE.format_hint(*hint)
+
+        return 0
+
+
+def PLUGIN_ENTRY():
+    return AMIE()
